495b0fbcb74b8191308a57bf2ffece12005e35b9
Ian13 <Ian13@PC-Ian>
2025-01-02 21:47:50 +0100

    MCreator Merge commit 'd50735ea1eef96a8f1fb39cc6e9c1a88f4870e17' into 2b78975815c3d38bff5f694a0a41b697af79585b

--- src/main/java/wuzu/ian/sorcerersecrets/SorcererSecretsMod.java
+++ src/main/java/wuzu/ian/sorcerersecrets/SorcererSecretsMod.java
@@ -1,9 +1,5 @@
 package wuzu.ian.sorcerersecrets;
 
-import wuzu.ian.sorcerersecrets.init.SorcererSecretsModTabs;
-import wuzu.ian.sorcerersecrets.init.SorcererSecretsModItems;
-import wuzu.ian.sorcerersecrets.init.SorcererSecretsModBlocks;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -52,12 +48,6 @@ public class SorcererSecretsMod {
 		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		SorcererSecretsModBlocks.REGISTRY.register(bus);
-
-		SorcererSecretsModItems.REGISTRY.register(bus);
-
-		SorcererSecretsModTabs.REGISTRY.register(bus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
